d8e3dedaf7701e17ef5b4b86711e57df78616913
Le Viet Nam <levietnam2509@gmail.com>
2024-07-04 17:39:53 +0700

    fix

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(`mongodb+srv://admin:02081958nam@cluster0.hylfk.mongodb.net/cms?retryWrites=true&w=majority&appName=Cluster0`);
+        const conn = await mongoose.connect(`${process.env.MONGO_URI}`);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
